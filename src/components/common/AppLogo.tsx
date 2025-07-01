be24e4b4397e91d28d4148970435adab3125ca1b
七七 <nijgnoix@gmail.com>
2025-07-01 23:42:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppHeader>
    
    with

--- src/components/common/AppLogo.tsx
+++ src/components/common/AppLogo.tsx
@@ -4,7 +4,7 @@ export default function AppLogo() {
   return (
     <div className="flex items-center gap-2">
       <Tv2 className="h-6 w-6 text-primary" />
-      <span className="text-lg font-semibold text-foreground">晚风TV</span>
+      <span className="hidden sm:inline text-lg font-semibold text-foreground">晚风TV</span>
     </div>
   );
 }
