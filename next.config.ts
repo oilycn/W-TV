e1e97b907312ff12cf7164aeb27cbf93b5d12d10
七七 <nijgnoix@gmail.com>
2025-07-02 08:33:38 +0000

    还原

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,11 @@
 
 import type {NextConfig} from 'next';
 import withPWAInit from "@ducanh2912/next-pwa";
-import defaultRuntimeCaching from "@ducanh2912/next-pwa/cache";
 
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:m3u8|ts)$/i,
-      handler: 'NetworkOnly',
-      options: {
-        cacheName: 'streaming-media',
-      },
-    },
-    ...defaultRuntimeCaching,
-  ],
+  skipWaiting: true
 });
 
 const nextConfig: NextConfig = {
